cd7762804160dbbf5f0241b579113b3965634eaa
Ausar <namazov.h1@gmail.com>
2022-08-20 20:53:20 +0300

    second task

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const userTemrpature = prompt('Укажите температуру');
 const temprature = (9 / 5) * userTemrpature + 32;
 
 alert(temprature);
+
+//Second task
+const name = 'Василий';
+const admin = name;
+
+console.log(admin);
