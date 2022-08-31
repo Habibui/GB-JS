746cded8d05461f1055e69c65d04e56dae9c2355
Ausar <namazov.h1@gmail.com>
2022-08-31 14:56:59 +0300

    lesson-3-2

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ while (i <= 100) {
 
 // Задание 2
 
+let arrayBasket = [
+    ['apple', 1, 50],
+    ['pineapple', 2, 70],
+    ['banan', 1, 30],
+];
+
+function countBasketPrice() {
+    let totalSum = 0;
+    for (let index = 0; index < arrayBasket.length; index++) {
+        totalSum += arrayBasket[index][1] * arrayBasket[index][2];
+    }
+    console.log(totalSum);
+    return totalSum;
+}
+
+countBasketPrice();
