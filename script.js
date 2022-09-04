95ab80a69eef0010a1c7adcb41f7db3c6b069558
Ausar <namazov.h1@gmail.com>
2022-09-04 18:21:04 +0300

    lesson-4-2

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 // Задание 1
 
 /*
-let i = 2;
-
-while (i > 0 && i <= 100) {
-  if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
-    console.log(i);
-  } else if (i == 2 || i == 3 || i == 5 || i == 7) {
-    console.log(i);
+function myFn(userNumber) {
+  let myObj = {
+    units: 'единицы: ' + 
+    dozens: 'десятки: ' +
+    hundreds: 'сотни: ' +
   }
-  i++;
+
+  return myObj;
 }
 */
 
 // Задание 2
 
-let arrayBasket = [
-    ['apple', 1, 50],
-    ['pineapple', 2, 70],
-    ['banan', 1, 30],
-];
+let objBasket = [
+  {
+    name: 'apple',
+    count: 1,
+    price: 50
+  },
+  {
+    name: 'pineapple',
+    count: 2,
+    price: 70
+  },
+  {
+    name: 'banan',
+    count: 1,
+    price: 30
+  }
+]
 
 function countBasketPrice() {
-    let totalSum = 0;
-    for (let index = 0; index < arrayBasket.length; index++) {
-        totalSum += arrayBasket[index][1] * arrayBasket[index][2];
-    }
-    console.log(totalSum);
-    return totalSum;
+  let totalSum = 0;
+  for (let index = 0; index < objBasket.length; index++) {
+    totalSum += objBasket[index].count * objBasket[index].price;
+  }
+  console.log(totalSum);
+  return totalSum;
 }
 
 countBasketPrice();
