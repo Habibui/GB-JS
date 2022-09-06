a932d2fd2724021370d563d2b1048cce5ce7d00d
Ausar <namazov.h1@gmail.com>
2022-09-06 13:04:03 +0300

    lesson-4-1

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 // Задание 1
 
-/*
-function myFn(userNumber) {
-  let myObj = {
-    units: 'единицы: ' + 
-    dozens: 'десятки: ' +
-    hundreds: 'сотни: ' +
+/*function myFn(userNumber) {
+  if (userNumber < 999) {
+    const stats = {
+        'units': Math.floor(userNumber % 10),
+        'dozens': Math.floor(userNumber % 100 / 10),
+        'hundreds': Math.floor(userNumber / 100)
+        }
+        return stats;
+  } else {
+        console.log('введено некорректное число')
   }
+}*/
 
-  return myObj;
-}
-*/
 
 // Задание 2
 
-let objBasket = [
-  {
-    name: 'apple',
-    count: 1,
-    price: 50
-  },
-  {
-    name: 'pineapple',
-    count: 2,
-    price: 70
-  },
-  {
-    name: 'banan',
-    count: 1,
-    price: 30
-  }
-]
+let objBasket = [{
+  name: 'apple',
+  count: 1,
+  price: 50
+},
+{
+  name: 'pineapple',
+  count: 2,
+  price: 70
+},
+{
+  name: 'banan',
+  count: 1,
+  price: 30
+}]
 
 function countBasketPrice() {
   let totalSum = 0;
