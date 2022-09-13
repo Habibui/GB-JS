0c7ffa89df613ddcd80885804acad04bbc316ab5
Ausar <namazov.h1@gmail.com>
2022-09-13 17:10:34 +0300

    заглушка к 6 дз

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
 // Задание 1
 
-const gridStyle = `display: grid;
-    grid-template-columns : 20px  repeat(8, 50px);
-    grid-template-rows: 20px repeat(8, 50px);
-    justify-items: center;
-    align-items: center;`
-
-function makeChessBoard() {
-  const board = document.createElement('div')
-  board.style = gridStyle
-
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const cell = document.createElement('div')
-      if (i === 0 || j === 0) {
-        cell.innerText = (j) ? String.fromCharCode(64 + j) : i || ""
-      } else {
-        cell.style.width = "100%"
-        cell.style.height = "100%"
-        if (i % 2) cell.style.backgroundColor = j % 2 ? '#f1d264' : '#4f3e07'
-        else cell.style.backgroundColor = !(j % 2) ? '#f1d264' : '#4f3e07'
-      }
-      board.appendChild(cell)
-    }
-  }
-  document.body.prepend(board)
-}
-
-makeChessBoard()
-
-
-
-// Задание 2
-
-/*let obj = {
+let obj = {
   basket: [{
     name: 'apple',
     count: 1,
@@ -69,4 +36,4 @@ makeChessBoard()
   }
 }
 
-obj.basketRender();*/
+obj.basketRender();
